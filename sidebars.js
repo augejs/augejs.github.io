7d871f7b07f1276c03684115bb43e8d65877bce4
alex <alex@alex.com>
2020-09-06 13:10:17 +0800

    first commit

---
+++ sidebars.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+module.exports = {
+  docs: {
+    "Guide": [
+      'Guide/Introduction', 
+    ],
+    "Fundamentals": [
+    ],
+    "Techniques": [
+      'Techniques/Techniques-Schedule'
+    ],
+    "Community": [
+    ]
+  },
+};
